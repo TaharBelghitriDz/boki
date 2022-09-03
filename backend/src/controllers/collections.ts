4e63642132d3e2e8231ea3bdd88a2878fa466ce5
tahar belghitri <tahartlm0@gmail.com>
2022-09-03 07:56:22 +0100

    Update collections.ts

--- backend/src/controllers/collections.ts
+++ backend/src/controllers/collections.ts
@@ -29,25 +29,78 @@ export const createCollection: Handler = (req, res) => {
 };
 
 export const collectinActions: Handler = (req, res) => {
+  // need to validate the opiration
+  // (code check if there is a value and don't check if the value is on the db or no for example )
+  // it's working fine for simple idea like mine (no one gonna try to hack it ... it's open source already)
+
   const { collectionName, bookName, img, action } = req.body;
 
   const userInfos = (req as any).user as userSchema;
 
-  if (!action) return res.status(400).json({ err: "no action " });
+  const searchQuery = () => {
+    if (
+      (action === "create" && collectionName) ||
+      (action === "remove" && collectionName)
+    ) {
+      if (action == "remove" && bookName)
+        return { "collections.books.title": bookName };
+      if (action == "remove" && !bookName)
+        return { "collections.name": collectionName };
+      else if (bookName) return { "collections.name": collectionName };
 
-  if (
-    (action === "create" && collectionName && bookName && img) ||
-    (action === "remove" && (collectionName || bookName))
-  )
-    user
-      .find({ $and: [{ _id: userInfos._id }] })
-      .then((user) => {
-        if (!user) return;
-      })
-      .catch(
-        (err) => (
-          console.log(err),
-          res.status(400).json({ err: "something wrong happend #2" })
-        )
-      );
+      return {};
+    } else return;
+  };
+
+  if (!searchQuery())
+    return res.status(400).json({ err: "something wrong happend #1" });
+
+  user
+    .findOne({
+      $and: [{ _id: userInfos._id }, { ...searchQuery() }],
+    })
+    .then((user: any) => {
+      console.log(searchQuery());
+
+      if (!user)
+        return res.status(400).json({ err: "something wrong happend #3" });
+
+      if (action === "create" && !bookName)
+        user.collections.push({ name: collectionName, books: [] });
+
+      if (action === "create" && bookName && img)
+        user.collections.map((e: any) => {
+          if (e.name === collectionName)
+            return e.books.push({ title: bookName, img });
+        });
+
+      if (action === "remove" && !bookName)
+        user.collections = user.collections.filter(
+          (e: any) => e.name !== collectionName
+        );
+
+      if (action === "remove" && bookName)
+        user.collections.map((e: any) => {
+          if (e.name === collectionName)
+            return (e.books = e.books.filter((i: any) => i.title != bookName));
+        });
+
+      console.log(user.collections);
+
+      user
+        .save()
+        .then(() => res.status(200).json({ result: action }))
+        .catch(
+          (err: any) => (
+            console.log(err),
+            res.status(400).json({ err: "somethiong wrong happend #1" })
+          )
+        );
+    })
+    .catch(
+      (err) => (
+        console.log(err),
+        res.status(400).json({ err: "something wrong happend #2" })
+      )
+    );
 };
